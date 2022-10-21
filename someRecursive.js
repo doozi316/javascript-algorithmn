42044675fad4eb143a5d0e29a7c0e1a9316d4a0f
김현진 <doozi316@gmail.com>
2022-10-21 10:55:38 +0900

    someRecursive

---
+++ someRecursive.js
@@ -0,0 +1,12 @@
+function someRecursive(arr, callback) {
+    if (!arr.length) return false;
+    return callback(arr[0]) || someRecursive(arr.slice(1), callback);
+}
+
+// SAMPLE INPUT / OUTPUT
+const isOdd = (val) => val % 2 !== 0;
+
+console.log(someRecursive([1, 2, 3, 4], isOdd)); // true
+console.log(someRecursive([4, 6, 8, 9], isOdd)); // true
+console.log(someRecursive([4, 6, 8], isOdd)); // false
+console.log(someRecursive([4, 6, 8], (val) => val > 10)); // false
