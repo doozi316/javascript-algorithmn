aaea6873ad2dd8b7d633d476f2fa61f451f21f45
김현진 <doozi316@gmail.com>
2022-11-03 13:55:34 +0900

    solutions

---
+++ collectStringsSolution2.js
@@ -0,0 +1,11 @@
+function collectStringsSolution2(obj) {
+    var stringsArr = [];
+    for (var key in obj) {
+        if (typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        } else if (typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+    return stringsArr;
+}
